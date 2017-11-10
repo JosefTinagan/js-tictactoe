bb1f3d0622ed2b9b6f6a6e9fda589e78a9eea4c4
Josef Perry Tinagan <josephperry_06@yahoo.com>
2017-11-10 19:34:20 +0800

    added constructors for cell and board

--- js/main.js
+++ js/main.js
@@ -1,8 +1,64 @@
 $(document).ready(function(){
 
+
+	function Cell(value=''){
+	  this.value = value;
+
+	}
+	//Add Methods to the constructor Cell
+	Cell.prototype = {
+	  constructor: Cell,
+	  changeValue: function(new_value){
+	    this.value = new_value;
+	  }
+	}
+
+	function Board(){
+	  this.value = '';
+
+	}
+
+	Board.prototype = {
+	  constructor: Board,
+
+	  createBoard: function(){
+	    var temp = [];
+	    for(var i = 0; i < 9; i++){
+	      temp.push(new Cell());
+	    }
+	    this.value = temp;
+	  },
+
+	  showBoard: function(){
+	    for(var i = 0; i < 9; i++){
+	      console.log(this.value[i]);
+	    }
+	  }
+	}
+
+	var $grid = {
+	  value: '',
+	  showGrid: function(){
+
+	  },
+	  createGrid: function(){
+	  	var x = 9;
+	    for(var i = 0; i < x; i++){
+	      $cell.new
+	    }
+	  }
+	}
+
 	$('.test').on('click',doSomething);
 
 	function doSomething(){
-	  alert('test');
+	  var x = new Cell ('beybi');
+	  console.log(x.value);
+	  x.changeValue('beyyb');
+	  console.log(x.value);
+	  var y = new Board();
+	  y.createBoard();
+	  console.log(y);
+	  y.showBoard();
 	}
 });
